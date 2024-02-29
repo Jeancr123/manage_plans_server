9885898176f2f36fd8c9d6417c5c1c5832b50ae1
Jean <jean_henriquez@hotmail.com>
2024-02-29 11:28:47 -0700

    accessing db with raw string

--- database.js
+++ database.js
@@ -2,11 +2,11 @@ const { Sequelize} = require('sequelize');
 
 const sequelize = new Sequelize({
     dialect: `${process.env.DB_TYPE}`,
-    host: `${String.raw(process.env.DB_HOST)}`,
+    host: `${process.env.DB_HOST}`,
     port: process.env.DB_PORT, 
-    database: `${String.raw(process.env.DB_NAME)}`,
+    database: `${process.env.DB_NAME}`,
     username: `${process.env.DB_USER}`,
-    password: `${String.raw(process.env.DB_PASSWORD)}`,
+    password: `${process.env.DB_PASSWORD}`,
     dialectOptions: {
       ssl: {
         rejectUnauthorized: false, 
